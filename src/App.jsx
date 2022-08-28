6c9dcf457aaf54a4423d866aed3cdd67d3577e2e
Chaos <antonioscutari2@hotmail.com>
2022-08-28 03:09:31 -0300

    [CONN DONE]

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Creator from "./pages/Creator";
+import Details from "./pages/Details";
+import Home from "./pages/Home";
+import Landing from "./pages/Landing";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import axios from "axios";
+
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/details/:id" component={Details} />
+          <Route exact path="/create" component={Creator} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
