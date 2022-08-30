f548909a83add48afeb390627dc88e26f54f844a
Chaos <antonioscutari2@hotmail.com>
2022-08-29 23:30:13 -0300

    [TRY] Change background color per pokemon type

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,14 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { getPokemons } from "../redux/actions";
 import { Link } from "react-router-dom";
-// import "../styles";
+import "../styles/components.css";
+import colors from "../styles/colors.js";
 export let missingno =
   "https://static.wikia.nocookie.net/espokemon/images/4/41/Mimikyu.png";
 
 const Cards = ({ currentPokes }) => {
   const regex = new RegExp("[a-z]");
   const dispatch = useDispatch();
+  // eslint-disable-next-line no-unused-vars
+  // const [color, setColor] = useState();
+
   useEffect(() => {
     dispatch(getPokemons());
   }, [dispatch]);
@@ -16,7 +20,11 @@ const Cards = ({ currentPokes }) => {
     return (
       <>
         {currentPokes?.map((p) => (
-          <div className="card" key={p.id}>
+          <div
+            className="card"
+            key={p.id}
+            // style={{ backgroundColor: setColor }}
+          >
             <div className="cardInfo">
               <h1>#{regex.test(p.id) === true ? "DB" : p.id}</h1>
               <Link
