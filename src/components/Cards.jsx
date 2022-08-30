9b311ca65eac84a6e8e41a2a68683b7958d8cad3
Chaos <antonioscutari2@hotmail.com>
2022-08-30 00:54:24 -0300

    [SUCCESS] BgColor for each type

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { getPokemons } from "../redux/actions";
 import { Link } from "react-router-dom";
+import { Loader } from "../pages/Loader";
 import "../styles/components.css";
 import colors from "../styles/colors.js";
 export let missingno =
@@ -9,10 +10,15 @@ export let missingno =
 
 const Cards = ({ currentPokes }) => {
   const regex = new RegExp("[a-z]");
+  const comesFromDb = (item) => {
+    regex.test(item);
+  };
   const dispatch = useDispatch();
-  // eslint-disable-next-line no-unused-vars
-  // const [color, setColor] = useState();
-
+  const bGcolour = (poketype) => {
+    if (colors.hasOwnProperty(poketype)) {
+      return { backgroundColor: colors[poketype] };
+    }
+  };
   useEffect(() => {
     dispatch(getPokemons());
   }, [dispatch]);
@@ -23,10 +29,12 @@ const Cards = ({ currentPokes }) => {
           <div
             className="card"
             key={p.id}
-            // style={{ backgroundColor: setColor }}
+            style={bGcolour(
+              comesFromDb(p.id) === true ? p.types[0].name : p.types[0]
+            )}
           >
             <div className="cardInfo">
-              <h1>#{regex.test(p.id) === true ? "DB" : p.id}</h1>
+              <h1>#{comesFromDb(p.id) === true ? "DB" : p.id}</h1>
               <Link
                 style={{ textDecoration: "none", color: "black" }}
                 to={"/details/" + p.id}
@@ -34,13 +42,28 @@ const Cards = ({ currentPokes }) => {
                 <h2>{p.name}</h2>
               </Link>
             </div>
+            {comesFromDb(p.id) === true
+              ? p?.types.map((t) => (
+                  <span
+                    className="typeSpan"
+                    style={bGcolour(t.name)}
+                    key={t.name + p.id + "DB"}
+                  >
+                    {t.name}
+                  </span>
+                ))
+              : p?.types.map((t) => (
+                  <span className="typeSpan" style={bGcolour(t)} key={t + p.id}>
+                    {t}
+                  </span>
+                ))}
             <img src={p.image ? p.image : missingno} alt="cardimgerror" />
           </div>
         ))}
       </>
     );
   } else {
-    return "nothing";
+    return <Loader />;
   }
 };
 export default Cards;
