cc5828270bd4e9d7f8341940710b16c56fd8e00f
Chaos <antonioscutari2@hotmail.com>
2022-09-10 23:20:08 -0300

    [CHANGE] in import Loader and the MissingNo

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,11 +2,11 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { getPokemons } from "../redux/actions";
 import { Link } from "react-router-dom";
-import { Loader } from "../pages/Loader";
+import Loader from "../pages/Loader";
 import "../styles/components.css";
 import colors from "../styles/colors.js";
 export let missingno =
-  "https://static.wikia.nocookie.net/espokemon/images/4/41/Mimikyu.png";
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/MissingNo.png/256px-MissingNo.png?20211220044128";
 
 const Cards = ({ currentPokes }) => {
   const regex = new RegExp("[a-z]");
