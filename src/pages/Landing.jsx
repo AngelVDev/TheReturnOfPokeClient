c46fc782e53becdfd4ac905c9821e3b85b3432f9
Chaos <antonioscutari2@hotmail.com>
2022-09-12 23:54:38 -0300

    [ADD] Landing pseudo-style with Date

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import vidDay from "../assets/vidDay.mp4";
+import vidNite from "../assets/vidNite.mp4";
 
 const Landing = () => {
+  const date = new Date();
   return (
-    <div>
-      Landing
-      <Link to={"/home/"}>
+    <div className="bgLanding">
+      <video style={{ width: "100vw", zIndex: -1 }} loop autoPlay>
+        <source
+          src={
+            date.getHours() < 7 || date.getHours() > 19
+              ? vidNite
+              : date.getHours() >= 7 || date.getHours() < 19
+              ? vidDay
+              : null
+          }
+          type="video/mp4"
+        />
+        Your browser does not support the video tag.
+      </video>
+      <Link
+        style={{ textDecoration: "none", zIndex: 1, position: "static" }}
+        to={"/home/"}
+      >
         <button>Apretame</button>
       </Link>
     </div>
