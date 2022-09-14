76538ea153199dee412d71fb17e15da14b9bd540
Chaos <antonioscutari2@hotmail.com>
2022-09-14 03:21:35 -0300

    [ADD] Gifs and [STYLE : done]

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,30 +1,56 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import vidDay from "../assets/vidDay.mp4";
-import vidNite from "../assets/vidNite.mp4";
+import vidDay from "../assets/vidDay.gif";
+import vidNite from "../assets/vidNite.gif";
 
 const Landing = () => {
   const date = new Date();
+  const bgColour = (date) => {
+    if (date.getHours() < 7 || date.getHours() > 19) {
+      return {
+        background:
+          "linear-gradient(139deg, #95CA6D 18%, #C75486 53%, #5D0059 100%)",
+      };
+    } else {
+      return {
+        background:
+          "linear-gradient(139deg, #4E6BF1 14%, rgba(143, 147, 255, 0.49) 54%, #61107D 90%)",
+      };
+    }
+  };
   return (
-    <div className="bgLanding">
-      <video style={{ width: "100vw", zIndex: -1 }} loop autoPlay>
-        <source
-          src={
-            date.getHours() < 7 || date.getHours() > 19
-              ? vidNite
-              : date.getHours() >= 7 || date.getHours() < 19
-              ? vidDay
-              : null
-          }
-          type="video/mp4"
-        />
-        Your browser does not support the video tag.
-      </video>
+    <div className="bgLanding" style={bgColour(date)}>
+      <img
+        className="pokeLogo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/2560px-International_Pok%C3%A9mon_logo.svg.png"
+        alt="pokeLogo"
+      />
+      <h2 className="h2Landing">
+        ¡There are many pokémons waiting for you in this site!
+      </h2>
+      <img
+        className="landingVideo"
+        src={
+          date.getHours() < 7 || date.getHours() > 19
+            ? vidNite
+            : date.getHours() >= 7 || date.getHours() < 19
+            ? vidDay
+            : null
+        }
+        alt="background failed"
+      ></img>
+
       <Link
-        style={{ textDecoration: "none", zIndex: 1, position: "static" }}
+        style={{
+          textDecoration: "none",
+          zIndex: 1,
+          position: "absolute",
+          maxHeight: "5vh",
+          display: "contents",
+        }}
         to={"/home/"}
       >
-        <button>Apretame</button>
+        <button className="landingBtn">Let's check</button>
       </Link>
     </div>
   );
